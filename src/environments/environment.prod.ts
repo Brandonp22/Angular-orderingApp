cc094e66b5f1e096dad8c71f9f3481951e2e23e6
bjpc <brandonpineda4@gmail.com>
2022-04-22 14:50:16 -0600

    solucionando errores

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  api_url: 'http://localhost:1337',
+  cp: {
+    api_key: '5579980505863a3f6aabd82.89189525',
+    site_id: 659913,
+    notify_url: 'https://YOUR_NOTIFY_URL',
+    currency: 'CFA'
+  },
+  firebase : {
+    databaseURL: "https://bocadillos-chiqui-default-rtdb.firebaseio.com",
+    apiKey: "AIzaSyChuDbtOh7Dy6FRsnvtNLx3gmEjTGezue4",
+    authDomain: "bocadillos-chiqui.firebaseapp.com",
+    projectId: "bocadillos-chiqui",
+    storageBucket: "bocadillos-chiqui.appspot.com",
+    messagingSenderId: "701332314169",
+    appId: "1:701332314169:web:794bdadc85c34a6d9655e8",
+    measurementId: "G-7QRJCWL5CR"
+  }
+};
